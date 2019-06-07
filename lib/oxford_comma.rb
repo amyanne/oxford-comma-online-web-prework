f7384eee8668739637be6f2e5d3775f7dd9750f6
Amy Westcott <amy.westcott@yahoo.com>
2019-06-07 20:27:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
+  i = 0
   if array.length == 1 
     array.join
   elsif array.length == 2 
   array.join(" and ")
 else 
- string = array.join(", ")
-  last = string.split.last 
-  string.delete.last(" ")
-  string << "and"
-  string.push(last)
+ while i < array.length - 1
+ array.join(", ")
+end
+array.join(", and ")
 end
 
 end
